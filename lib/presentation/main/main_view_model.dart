061928414fe94517dfc5dbd9de37d4373e4a96f6
shin-SeungHun <sng7387@gmail.com>
2024-01-11 00:53:52 +0900

    create main_view_model

---
+++ lib/presentation/main/main_view_model.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_weather_app/data/repository/weather_repository_impl.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
+
+import '../../data/dto/weather_dto.dart';
+import '../../domain/model/weather_item.dart';
+
+class MainViewModel extends ChangeNotifier {
+  final _repository = WeatherRepositoryImpl();
+  List<String>? _timeList = [];
+  List<num>? _tempList = [];
+  List<num>? _humidityList = [];
+  List<num>? _weatherCodeList = [];
+  List<num>? _windSpeedList = [];
+  List<num>? _pressureList = [];
+
+  List<String>? get timeList => _timeList;
+
+  List<num>? get tempList => _tempList;
+
+  List<num>? get humidityList => _humidityList;
+
+  List<num>? get weatherCodeList => _weatherCodeList;
+
+  List<num>? get windSpeedList => _windSpeedList;
+
+  List<num>? get pressureList => _pressureList;
+
+  bool isLoading = false;
+
+  Future<void> fetchWeathers() async {
+    isLoading = true;
+    notifyListeners();
+
+    WeatherItem weatherItem = await _repository.getWeatherItems(
+        36.373868762572464, 127.31794258531129);
+    _timeList = weatherItem.time;
+    _tempList = weatherItem.temperature;
+    _humidityList = weatherItem.relativeHumidity;
+    _weatherCodeList = weatherItem.weatherCode;
+    _windSpeedList = weatherItem.windSpeed;
+    _pressureList = weatherItem.pressure;
+
+    isLoading = false;
+    notifyListeners();
+  }
+
+  String convertTimeList(int i) {
+    initializeDateFormatting('ko_KR', null);
+
+    if (timeList != null && i < timeList!.length) {
+      DateTime date = DateTime.parse(timeList![i]);
+      DateFormat formatter = DateFormat('yyyy년 M월 d일 EEEE H시', 'ko_KR');
+
+      return formatter.format(date);
+    }
+
+    return 'N/A';
+  }
+
+  String convertTime(int i) {
+    initializeDateFormatting('ko_KR', null);
+
+    if (timeList != null && i < timeList!.length) {
+      DateTime date = DateTime.parse(timeList![i]);
+      String formatter = DateFormat('a h시', 'ko_KR').format(date);
+
+      return formatter;
+    }
+
+    return 'N/A';
+  }
+}
