1976a8eeeed9842219fa63ff5045a52dd088f84c
shin-SeungHun <sng7387@gmail.com>
2024-01-11 00:51:09 +0900

    refactor: weather_api

--- lib/data/data_source/weather_api.dart
+++ lib/data/data_source/weather_api.dart
@@ -2,12 +2,16 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
+import '../dto/weather_dto.dart';
+
 class WeatherApi {
-  Future<Map<String, dynamic>> getWeatherInfoJson() async {
+ Future<WeatherDto> getWeatherInfoJson(double lat, double long) async {
     final response = await http.get(
       Uri.parse(
-          'https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,weathercode,relativehumidity_2m,windspeed_10m,pressure_msl&latitude=36.373868762572464&longitude=127.31794258531129'),
+          'https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,weathercode,relativehumidity_2m,windspeed_10m,pressure_msl&latitude=$lat&longitude=$long'),
     );
-    return jsonDecode(response.body);
+    // 36.373868762572464
+    // 127.31794258531129
+    return WeatherDto.fromJson(jsonDecode(response.body));
   }
 }
