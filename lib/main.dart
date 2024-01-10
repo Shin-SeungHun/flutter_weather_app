b3b1f342c2b89e9c392b4ea5700483408a31470b
shin-SeungHun <sng7387@gmail.com>
2024-01-11 00:54:43 +0900

    add ChangeNotifierProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_weather_app/presentation/main/main_screen.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:provider/provider.dart';
+import 'presentation/main/main_view_model.dart';
 
-void main() {
+void main() async {
+  // WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('ko_KR', null);
   runApp(const MyApp());
 }
 
@@ -14,6 +20,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+      ),
+      home: ChangeNotifierProvider.value(
+        value: MainViewModel()..fetchWeathers(),
+        child: const MainScreen(),
       ),
     );
   }
