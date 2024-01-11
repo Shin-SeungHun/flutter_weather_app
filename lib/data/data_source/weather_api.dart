d1f46ab5bd86c369053a8c67dbfb8c70f2d6e4d9
shin-SeungHun <sng7387@gmail.com>
2024-01-11 14:08:07 +0900

    refactor: add required

--- lib/data/data_source/weather_api.dart
+++ lib/data/data_source/weather_api.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as http;
 import '../dto/weather_dto.dart';
 
 class WeatherApi {
- Future<WeatherDto> getWeatherInfoJson(double lat, double long) async {
+ Future<WeatherDto> getWeatherInfoJson({required num lat,required num long}) async {
     final response = await http.get(
       Uri.parse(
           'https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,weathercode,relativehumidity_2m,windspeed_10m,pressure_msl&latitude=$lat&longitude=$long'),
