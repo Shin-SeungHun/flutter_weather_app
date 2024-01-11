c3d3fed5db8a20dc39d0ab55217f7ed273d094c7
shin-SeungHun <sng7387@gmail.com>
2024-01-11 14:22:59 +0900

    refactor: delete !

--- lib/presentation/main/main_view_model.dart
+++ lib/presentation/main/main_view_model.dart
@@ -2,30 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_weather_app/data/repository/weather_repository_impl.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
-
-import '../../data/dto/weather_dto.dart';
 import '../../domain/model/weather_item.dart';
 
 class MainViewModel extends ChangeNotifier {
   final _repository = WeatherRepositoryImpl();
-  List<String>? _timeList = [];
-  List<num>? _tempList = [];
-  List<num>? _humidityList = [];
-  List<num>? _weatherCodeList = [];
-  List<num>? _windSpeedList = [];
-  List<num>? _pressureList = [];
+  List<String> _timeList = [];
+  List<num> _tempList = [];
+  List<num> _humidityList = [];
+  List<num> _weatherCodeList = [];
+  List<num> _windSpeedList = [];
+  List<num> _pressureList = [];
 
-  List<String>? get timeList => _timeList;
-
-  List<num>? get tempList => _tempList;
-
-  List<num>? get humidityList => _humidityList;
-
-  List<num>? get weatherCodeList => _weatherCodeList;
-
-  List<num>? get windSpeedList => _windSpeedList;
-
-  List<num>? get pressureList => _pressureList;
 
   bool isLoading = false;
 
@@ -34,7 +21,9 @@ class MainViewModel extends ChangeNotifier {
     notifyListeners();
 
     WeatherItem weatherItem = await _repository.getWeatherItems(
-        36.373868762572464, 127.31794258531129);
+      lat: 36.373868762572464,
+      long: 127.31794258531129,
+    );
     _timeList = weatherItem.time;
     _tempList = weatherItem.temperature;
     _humidityList = weatherItem.relativeHumidity;
@@ -46,11 +35,11 @@ class MainViewModel extends ChangeNotifier {
     notifyListeners();
   }
 
-  String convertTimeList(int i) {
+  String convertTimeList({required int i}) {
     initializeDateFormatting('ko_KR', null);
 
-    if (timeList != null && i < timeList!.length) {
-      DateTime date = DateTime.parse(timeList![i]);
+    if (i < timeList.length) {
+      DateTime date = DateTime.parse(timeList[i]);
       DateFormat formatter = DateFormat('yyyy년 M월 d일 EEEE H시', 'ko_KR');
 
       return formatter.format(date);
@@ -59,11 +48,11 @@ class MainViewModel extends ChangeNotifier {
     return 'N/A';
   }
 
-  String convertTime(int i) {
+  String convertTime({required int i}) {
     initializeDateFormatting('ko_KR', null);
 
-    if (timeList != null && i < timeList!.length) {
-      DateTime date = DateTime.parse(timeList![i]);
+    if (i < timeList.length) {
+      DateTime date = DateTime.parse(timeList[i]);
       String formatter = DateFormat('a h시', 'ko_KR').format(date);
 
       return formatter;
@@ -71,4 +60,16 @@ class MainViewModel extends ChangeNotifier {
 
     return 'N/A';
   }
+
+  List<String> get timeList => _timeList;
+
+  List<num> get tempList => _tempList;
+
+  List<num> get humidityList => _humidityList;
+
+  List<num> get weatherCodeList => _weatherCodeList;
+
+  List<num> get windSpeedList => _windSpeedList;
+
+  List<num> get pressureList => _pressureList;
 }
