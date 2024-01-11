69f7bb3c4bbce7fc4d9ba3f853566528786835d7
shin-SeungHun <sng7387@gmail.com>
2024-01-11 14:20:15 +0900

    refactor: add required

--- lib/domain/repository/weather_repository.dart
+++ lib/domain/repository/weather_repository.dart
@@ -2,5 +2,5 @@ import '../../data/dto/weather_dto.dart';
 import '../model/weather_item.dart';
 
 abstract interface class WeatherRepository{
-  Future<WeatherItem> getWeatherItems(double lat, double long);
+  Future<WeatherItem> getWeatherItems({required num lat,required num long});
 }
